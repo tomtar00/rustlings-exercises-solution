e56533ab9fe7b1b51222c7faa42d1854a54f2e38
tomtar00 <t.tarnowski33@gmail.com>
2024-05-16 11:12:33 +0200

    init

---
+++ clippy/target/debug/deps/clippy3-5ef86612d9331dd9.d
@@ -0,0 +1,9 @@
+/Users/tomtar/dev/rustlings/exercises/clippy/target/debug/deps/libclippy3-5ef86612d9331dd9.rmeta: clippy3.rs Cargo.toml
+
+/Users/tomtar/dev/rustlings/exercises/clippy/target/debug/deps/clippy3-5ef86612d9331dd9.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
